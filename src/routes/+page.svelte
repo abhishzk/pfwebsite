143232fe960731bd15ef95188f1b39566927a2ec
abhishzk <abhishzk.dev@gmail.com>
2026-01-07 12:40:38 +0000

    Format Code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -60,11 +60,11 @@
 				class="mr-1"
 				aria-hidden="true"
 			>
-				<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-				<polyline points="14 2 14 8 20 8"></polyline>
-				<line x1="16" y1="13" x2="8" y2="13"></line>
-				<line x1="16" y1="17" x2="8" y2="17"></line>
-				<polyline points="10 9 9 9 8 9"></polyline>
+				<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+				<polyline points="14 2 14 8 20 8" />
+				<line x1="16" y1="13" x2="8" y2="13" />
+				<line x1="16" y1="17" x2="8" y2="17" />
+				<polyline points="10 9 9 9 8 9" />
 			</svg>
 			<span class="hidden sm:inline">View Resume</span>
 			<span class="sm:hidden">Resume</span>
@@ -89,9 +89,9 @@
 				class="mr-1"
 				aria-hidden="true"
 			>
-				<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-				<polyline points="7 10 12 15 17 10"></polyline>
-				<line x1="12" y1="15" x2="12" y2="3"></line>
+				<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+				<polyline points="7 10 12 15 17 10" />
+				<line x1="12" y1="15" x2="12" y2="3" />
 			</svg>
 			<span class="hidden sm:inline">Download</span>
 			<span class="sm:hidden">PDF</span>
