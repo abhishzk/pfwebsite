bc21149e0dd694ce7adb551cd7cec2ef663e86b2
Abhishek Kumar <abhishek.kumar@wattfootprint.com>
2026-01-07 12:48:44 +0000

    Remove resume view and download buttons from Hero and page components to streamline the interface.

--- src/lib/Home/Hero.svelte
+++ src/lib/Home/Hero.svelte
@@ -70,19 +70,6 @@
 
 	let heroContainer: HTMLDivElement | null = null;
 
-	const resumePath = '/resume.pdf';
-
-	function handleViewResume() {
-		// Click tracking hook - can be extended with analytics
-		window.open(resumePath, '_blank', 'noopener,noreferrer');
-	}
-
-	function handleDownloadResume(event: MouseEvent) {
-		// Click tracking hook - can be extended with analytics
-		// The download will happen via the anchor's download attribute
-		// This function is here for potential tracking
-	}
-
 	onMount(() => {
 		let i = 0;
 		const codeBlurb = '\tProduct Management, Engineering, Economics, Reader...';
@@ -166,61 +153,6 @@
 				<pre data-prefix="2"><code>{'{'}</code></pre>
 				<pre data-prefix="3"><code id="typewriter" class="text-success" /></pre>
 				<pre data-prefix="4"><code>{'}'}</code></pre>
-			</div>
-			<div class="flex flex-col sm:flex-row gap-4 mt-6 justify-center items-center">
-				<button
-					on:click={handleViewResume}
-					class="btn btn-primary"
-					aria-label="View resume PDF in new tab"
-					type="button"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="mr-2"
-						aria-hidden="true"
-					>
-						<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-						<polyline points="14 2 14 8 20 8"></polyline>
-						<line x1="16" y1="13" x2="8" y2="13"></line>
-						<line x1="16" y1="17" x2="8" y2="17"></line>
-						<polyline points="10 9 9 9 8 9"></polyline>
-					</svg>
-					View Resume
-				</button>
-				<a
-					href={resumePath}
-					download="Abhishek_Kumar_Resume.pdf"
-					on:click={handleDownloadResume}
-					class="btn btn-outline"
-					aria-label="Download resume PDF file"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 24 24"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="2"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						class="mr-2"
-						aria-hidden="true"
-					>
-						<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-						<polyline points="7 10 12 15 17 10"></polyline>
-						<line x1="12" y1="15" x2="12" y2="3"></line>
-					</svg>
-					Download Resume (PDF)
-				</a>
 			</div>
 		</div>
 	</div>
